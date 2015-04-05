39975ee12f149e29097ae4d10d7600463ac2557f
marvio.bezerra <marvio.bezerra@gmail.com>
2015-04-05 20:49:49 -0300

    First commit

---
+++ KnockoutAutoMap/Scripts/komap.js
@@ -0,0 +1,54 @@
+﻿/// <reference path="knockout-3.3.0.debug.js" />
+
+ko.autoMap = {
+    map: function (jsProperty, observableProperty) {
+
+        if (jsProperty instanceof Array) {
+            this.mapArray(jsProperty, observableProperty);
+            return;
+        }
+
+        var map = {};
+
+        for (prop in jsProperty) {
+
+            if (jsProperty[prop] instanceof Function) {
+                continue;
+            }
+
+            if (jsProperty[prop] instanceof Array) {
+                map[prop] = this.mapArrayToNew(jsProperty[prop]);
+                continue;
+            }
+
+            if (jsProperty[prop] instanceof Object) {
+                map[prop] = this.mapToNew(jsProperty[prop])();
+                continue;
+            }
+
+            map[prop] = new ko.observable(jsProperty[prop]);
+        }
+
+        observableProperty(map);
+    },
+    mapToNew: function (jsProperty) {
+        var observableReturn = new ko.observable();
+        this.map(jsProperty, observableReturn);
+        return observableReturn;
+    },
+    mapArray: function (jsArray, observableArray) {
+        observableArray.removeAll();
+
+        for (var i = 0; i < jsArray.length; i++) {
+            var item = new ko.observable();
+            this.map(jsArray[i], item);
+            observableArray.push(item());
+        }
+    },
+    mapArrayToNew: function (jsArray) {
+        var observableArray = new ko.observableArray();
+        this.mapArray(jsArray, observableArray);
+
+        return observableArray;
+    }
+}
---
+++ KnockoutAutoMap/Scripts/testViewModel.js
@@ -0,0 +1,50 @@
+﻿/// <reference path="knockout-3.3.0.debug.js" />
+/// <reference path="komap.js" />
+
+
+function testViewModel() {
+    var self = this;
+
+    var simpleObject = {
+        Key: 1,
+        Name: "Márvio André",
+        Email: "marvio.bezerra@gmail.com"
+    };
+
+    var complexObject = {
+        Key: 1,
+        Name: "Márvio André",
+        Email: "marvio.bezerra@gmail.com",
+        Job: {
+            Key: 101,
+            Type: "Developer",
+            Company: {
+                Key: 23,
+                Name: "Foo Tecnology",
+                Adress: {
+                    Key: 55,
+                    Country: "Brasil"
+                }
+            }
+        }
+    }
+
+    var arrayObject = [
+        { Key: 1, Name: "Item 1" },
+        { Key: 2, Name: "Item 2" },
+        { Key: 3, Name: "Item 3" },
+        { Key: 4, Name: "Item 4" },
+    ];
+
+    self.SimpleObject = new ko.observable();
+    self.ComplexObject = new ko.observable();
+    self.ArrayObject = new ko.observableArray();
+
+    self.Init = function () {
+        ko.autoMap.map(simpleObject, self.SimpleObject);
+        ko.autoMap.map(complexObject, self.ComplexObject);
+        ko.autoMap.map(arrayObject, self.ArrayObject);
+    }
+}
+
+ko.applyBindings(new testViewModel());
